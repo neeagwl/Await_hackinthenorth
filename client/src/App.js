08b21bf40ccb16089ecb0d6c6a3a6aa5e7a2818b
Harshal Raikwar <64153988+Harshal0902@users.noreply.github.com>
2020-12-19 18:47:57 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,18 +7,28 @@ import { About } from './About';
 import Home from './Home';
 import LogIn from './components/LogIn'
 import SignUp from './components/SignUp'
+import { NavigationBarPatient } from './components/Patients/NavigationBar';
+import HomePatients from './components/Patients/Home'
+import Details from './components/Patients/Details'
+import { NavigationBarDoctor } from './components/Doctors/NavigationBar';
+import HomeDoctor from './components/Doctors/Home'
+
 
 function App() {
   return (
     <React.Fragment>
       <Router>
         <NavigationBar />
+    
+     {/* <NavigationBarPatient /> */}
 
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/components/LogIn" component={LogIn} />
           <Route path="/components/SignUp" component={SignUp} />
+          <Route path="/components/Patients/Home" component={HomePatients} />
+          <Route path="/components/Patients/Details" component={Details} />
         </Switch>
       </Router>
     </React.Fragment>
